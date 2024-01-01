77c3f6d450000dcc1554a4b3e4bcbfad210b35d2
Weiberle17 <maxweiberle63@gmail.com>
2024-01-01 12:02:08 +0100

    feat: MinHeap

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,76 @@
 export default class MinHeap {
-    public length: number;
+  public length: number;
+  private data: number[];
+  constructor() {
+    this.data = [];
+    this.length = 0;
+  }
 
-    
+  insert(value: number): void {
+    this.data[this.length] = value;
+    this.heapifyUp(this.length);
+    this.length++;
+  }
+  delete(): number {
+    const out = this.data[0];
+    if (this.length === 0) {
+      return -1;
+    }
+    if (this.length === 1) {
+      this.data = [];
+      this.length--;
+      return out;
+    }
+    this.length--;
+    this.data[0] = this.data[this.length];
+    this.heapifyDown(0);
+    return out;
+  }
 
-    constructor() {
+  private heapifyUp(idx: number): void {
+    if (idx === 0) {
+      return;
+    }
+    const parentIdx = this.parent(idx);
+    if (this.data[idx] < this.data[parentIdx]) {
+      const x = this.data[idx]
+      this.data[idx] = this.data[parentIdx];
+      this.data[parentIdx] = x;
+      this.heapifyUp(parentIdx);
+    }
+  }
+
+  private heapifyDown(idx: number): void {
+    const lIdx = this.leftChild(idx);
+    const rIdx = this.rightChild(idx);
+
+    if (idx >= this.length || lIdx >= this.length) {
+      return;
     }
 
-    insert(value: number): void {
+    const lV = this.data[lIdx];
+    const rV = this.data[rIdx];
+    const v = this.data[idx];
+    if (lV > rV && v > rV) {
+      this.data[idx] = rV;
+      this.data[rIdx] = v;
+      this.heapifyDown(rIdx);
+    } else if (rV > lV && v > lV) {
+      this.data[idx] = lV;
+      this.data[lIdx] = v;
+      this.heapifyDown(lIdx);
+    }
+  }
 
-}
-    delete(): number {
+  private parent(idx: number): number {
+    return Math.floor((idx - 1) / 2);
+  }
 
-}
+  private leftChild(idx: number): number {
+    return (2 * idx + 1);
+  }
+
+  private rightChild(idx: number): number {
+    return (2 * idx + 2);
+  }
 }
